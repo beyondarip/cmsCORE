2d9cc55f8929df698bf93cd655adf4792a6d01a2
beyondarip <ari2331280@itpln.ac.id>
2025-04-18 14:55:51 +0700

    update the endpoint

--- config/settings.py
+++ config/settings.py
@@ -106,10 +106,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
-#     'DEFAULT_RENDERER_CLASSES': (
-#     'rest_framework.renderers.JSONRenderer',
-# )
+    'DEFAULT_RENDERER_CLASSES': (
+    'rest_framework.renderers.JSONRenderer',
+)
 }
+
 # CORS settings (for development)
 CORS_ALLOW_ALL_ORIGINS = True  # Only in developmen
 # Add this to your settings.py
